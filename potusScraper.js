f600fd2d116005475113f63cc65398196cd1167d
yoichimichael <yoichi.michael.nagano@gmail.com>
2021-10-29 18:48:52 -0700

    debug query

--- potusScraper.js
+++ potusScraper.js
@@ -2,13 +2,13 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const url = 'https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States';
 
-
-rp(url)
-  .then(html => {
+rp(url).then(html => {
     const $ = cheerio.load(html);
-    console.log($('b > a', html).length);
-    // console.log($('b > a', html));
-  })
-  .catch(function(err){
-    //handle error
-  });
+    const presTable = $('.wikitable b > a');
+    const wikiUrls = [];
+    for (let i = 0; i <= 45; i++) {
+      const url = presTable[`${i}`].attribs.href;
+      wikiUrls.push(url);
+    }
+    console.log(wikiUrls)
+  }).catch(console.log);
